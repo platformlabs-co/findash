dd0c89ca2f9953ea75a2592a07857878093548df
bianca rosa <bia@platformlabs.co>
2024-12-11 22:13:43 +0000

    add get backend url method

--- dashboard/src/utils.ts
+++ dashboard/src/utils.ts
@@ -1,7 +1,4 @@
-export const isUserLoggedIn = (): boolean => {
-    // Implement your logic to check if the user is logged in
-    // For example, check if a token exists in localStorage
-    return (
-       false
-    );
-  };
+
+export const getBackendUrl = (): string => {
+   return process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
+}
--- dashboard/src/views/admin/default/components/VendorMetrics.tsx
+++ dashboard/src/views/admin/default/components/VendorMetrics.tsx
@@ -2,18 +2,20 @@ import Card from "components/card";
 import ComplexTable from "./ComplexTable";
 import tableDataComplex from "./../variables/tableDataComplex"
 import { useAuth0 } from "@auth0/auth0-react";
+import { getBackendUrl } from "utils";
 
 const VendorMetrics = (props: { vendorName: String }) => {
   const {  getAccessTokenSilently } = useAuth0();
   const token =  getAccessTokenSilently();
   
   // Make an HTTP request to the server and send the token in the Authorization header
-  const server_url = "http://localhost:8000/v1/vendors-metrics"
+  const server_url = getBackendUrl() + "/v1/vendors-metrics"
   const data = fetch(server_url, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
+  console.log(data);
   return (
     <Card extra="pb-10 p-[20px]">
       <h1 className="uppercase text-cemter font-bold title">{props.vendorName}</h1>
