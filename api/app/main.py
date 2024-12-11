4207c14aba446b7a364eecf9c45329c4c9b03784
bianca rosa <bia@platformlabs.co>
2024-12-11 22:13:35 +0000

    run api w/ gunicorn  & adds cors middleware

--- api/app/main.py
+++ api/app/main.py
@@ -6,11 +6,13 @@ logging.basicConfig(level=logging.DEBUG)
 from fastapi import Depends, FastAPI, Request
 from fastapi.responses import JSONResponse, RedirectResponse
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 from authlib.integrations.starlette_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-
 from app.routers import auth
 from app.helpers.auth import OAuth
+
+logger = logging.getLogger(__name__)
 
 
 def setup_app():
@@ -22,6 +24,16 @@ def setup_app():
     app = FastAPI()
     OAuth().register()
     app.add_middleware(SessionMiddleware, secret_key=env.get('SECRET_KEY'))
+
+    logger.info("Setting up CORS middleware") 
+    # Set up CORS middleware
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[env["REACT_APP_FRONTEND_URL"]],  # Replace with your specific URL
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     return app
 
