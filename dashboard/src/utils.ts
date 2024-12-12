20f7f60bf0c57e9cf7f52f987a28a50b76c367c6
bianca rosa <bia@platformlabs.co>
2024-12-12 19:25:49 +0000

    frontend: enhance auth handling

--- dashboard/src/utils.ts
+++ dashboard/src/utils.ts
@@ -1,4 +1,19 @@
 
-export const getBackendUrl = (): string => {
+
+const getBackendUrl = (): string => {
    return process.env.REACT_APP_BACKEND_URL || 'http://localhost:8080';
+}
+
+export const CallBackendService = async(path: string, getAccessTokenSilently: () => Promise<string>): Promise<string> => {
+  const token = await getAccessTokenSilently();
+  try {
+    const data = await fetch(getBackendUrl() + path, {
+      headers: {
+         Authorization: `Bearer ${token}`,
+      }
+    });
+    return data.json();  
+   } catch (error) {
+    console.error(error);
+  }
 }
--- dashboard/src/views/admin/default/components/VendorMetrics.tsx
+++ dashboard/src/views/admin/default/components/VendorMetrics.tsx
@@ -1,21 +1,29 @@
 import Card from "components/card";
+import React, { useEffect, useState } from 'react';
 import ComplexTable from "./ComplexTable";
 import tableDataComplex from "./../variables/tableDataComplex"
+import { CallBackendService } from "utils";
 import { useAuth0 } from "@auth0/auth0-react";
-import { getBackendUrl } from "utils";
 
 const VendorMetrics = (props: { vendorName: String }) => {
+  const [data, setData] = useState<string | null>(null);
   const {  getAccessTokenSilently } = useAuth0();
-  const token =  getAccessTokenSilently();
-  
-  // Make an HTTP request to the server and send the token in the Authorization header
-  const server_url = getBackendUrl() + "/v1/vendors-metrics"
-  const data = fetch(server_url, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  console.log(data);
+  const [error, setError] = useState<string | null>(null);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const backend_data = await CallBackendService("/v1/vendors-metrics", getAccessTokenSilently);
+        console.log(backend_data);
+        setData(backend_data);
+      } catch (error) {	
+        console.error(error);
+        setError('Failed to fetch data');
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <Card extra="pb-10 p-[20px]">
       <h1 className="uppercase text-cemter font-bold title">{props.vendorName}</h1>
