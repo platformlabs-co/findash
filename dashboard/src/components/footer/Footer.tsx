ae09185b97aa31b5d9dcfb3ebfd8fb9d7bedc4c5
bianca rosa <me@biancarosa.com.br>
2024-12-15 19:13:41 +0000

    remove heading from footer

--- dashboard/src/components/footer/Footer.tsx
+++ dashboard/src/components/footer/Footer.tsx
@@ -1,7 +1,6 @@
 const Footer = () => {
   return (
     <div className="flex w-full flex-col items-center justify-between px-1 pb-8 pt-3 lg:px-8 xl:flex-row">
-      <h5 className="mb-4 text-center text-sm font-medium text-gray-600 sm:!mb-0 md:text-lg">
       <div>
         <ul className="flex flex-wrap items-center gap-3 sm:flex-nowrap md:gap-10">
           <li>
