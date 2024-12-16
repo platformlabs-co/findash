32b40ecf93bd962a5e991ecd1b0441bcf1ce8fad
bianca rosa <me@biancarosa.com.br>
2024-12-16 03:48:13 +0000

    Assistant checkpoint: Updated Python version to 3.11
    Assistant generated file changes:
    - replit.nix: Switch to Python 3.11
    - api/pyproject.toml: Update Python version requirement
    
    ---
    
    User prompt:
    
    Let's use python3.11 then

--- replit.nix
+++ replit.nix
@@ -1,15 +1,6 @@
 
 {pkgs}: {
   deps = [
-    (pkgs.python313.override {
-      packageOverrides = self: super: {
-        python = super.python.override {
-          version = "3.13.0a5";
-        };
-      };
-    })
-    pkgs.python312Packages.pip
-    pkgs.python312Packages.flake8
-    pkgs.openssh
+    pkgs.python311
   ];
 }
