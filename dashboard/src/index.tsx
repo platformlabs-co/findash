ffc325984cab5b66df0f0bcf515155e90c0e8eda
Bianca Rosa <bia@platformlabs.co>
2025-01-04 15:51:15 -0300

    fix index.tsx

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 import { initGTM } from "./utils/gtm";
 import "./index.css";
 
+/// <reference types="react" />
+
 // Initialize GTM
 initGTM();
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
   <Auth0Provider
-    domain={process.env.REACT_APP_AUTH0_DOMAIN || ""}
-    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ""}
+    domain="platformlabs.us.auth0.com"
+    clientId="mlUVKKqaYLc0QBITyOXT1ev0a1D5v72l"
     authorizationParams={{
+      audience: 'https://findash.cloudbudget.ai',
       redirect_uri: window.location.origin,
     }}
   >
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Auth0Provider>,
-  document.getElementById("root")
+  </Auth0Provider>
 );
