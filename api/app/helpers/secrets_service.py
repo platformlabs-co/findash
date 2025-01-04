c89f24a45969e30b025bb1cdc0f02b2638a57340
Bianca Rosa <bia@platformlabs.co>
2025-01-04 15:30:48 -0300

    fix: line too long

--- api/app/helpers/secrets_service.py
+++ api/app/helpers/secrets_service.py
@@ -22,7 +22,8 @@ class SecretsService:
 
             if not all([client_id, client_secret, project_id]):
                 raise ValueError(
-                    "INFISICAL_CLIENT_ID and INFISICAL_CLIENT_SECRET, INFISICAL_PROJECT_ID must be set"
+                    """INFISICAL_CLIENT_ID and INFISICAL_CLIENT_SECRET, 
+                    INFISICAL_PROJECT_ID must be set"""
                 )
 
             try:
