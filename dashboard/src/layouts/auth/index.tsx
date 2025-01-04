f21650175ab0d2f209334efb179eaa9d2a953b30
Bianca Rosa <bia@platformlabs.co>
2025-01-04 15:17:12 -0300

    remove top padding

--- dashboard/src/layouts/auth/index.tsx
+++ dashboard/src/layouts/auth/index.tsx
@@ -7,7 +7,7 @@ export default function Auth() {
     <div className="relative h-full w-full bg-navy-900">
       <main className="mx-auto h-full min-h-screen">
         <div className="relative flex h-full">
-          <div className="mx-auto flex min-h-full w-full flex-col justify-start pt-12 md:max-w-[75%]">
+          <div className="mx-auto flex min-h-full w-full flex-col justify-start md:max-w-[75%]">
             <Routes>
               {routes.map((route, key) => {
                 if (route.layout === "/auth") {
